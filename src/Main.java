44e43328570dda9b9023ee71a5c309efb0125a64
kirillsavinkov <kirill.savinkov@sredasolutions.com>
2025-09-13 12:12:55 +0300

    learn1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        float f = 0.7777777777f;
+        System.out.println(f);
+        double f2 =  0.7777777777;
+        System.out.println(f2);
+    }
+}
